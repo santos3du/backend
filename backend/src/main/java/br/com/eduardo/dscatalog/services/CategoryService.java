5d6b7e97bee73c78e5f00565495f9e637988968c
Eduardo Alves dos Santos <santosedu@live.com>
2021-11-30 18:44:00 -0300

    Post category

--- backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
@@ -32,4 +32,13 @@ public class CategoryService {
         Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Id not found."));
         return new CategoryDTO(entity);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repo.save(entity);
+        return new CategoryDTO(entity);
+
+    }
 }
