1f1618bcb984b553ef748c9fbeca5ba74d031ec9
Eduardo Alves dos Santos <santosedu@live.com>
2021-11-30 17:12:34 -0300

    Tratamento de excecoes, custom exception

--- backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
@@ -3,11 +3,13 @@ package br.com.eduardo.dscatalog.services;
 import br.com.eduardo.dscatalog.dto.CategoryDTO;
 import br.com.eduardo.dscatalog.entities.Category;
 import br.com.eduardo.dscatalog.repositories.CategoryRepository;
+import br.com.eduardo.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,6 +24,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll(){
         List<Category> list = repo.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findByid(Long id) {
+        Optional<Category> obj = repo.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Id not found."));
+        return new CategoryDTO(entity);
     }
 }
