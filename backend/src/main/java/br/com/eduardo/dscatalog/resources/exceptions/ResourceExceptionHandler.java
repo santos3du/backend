f06b649e93f3f14bf8caed45c8f751a65e123d0b
Eduardo Alves dos Santos <santosedu@live.com>
2021-12-01 10:50:38 -0300

    Update category

--- backend/src/main/java/br/com/eduardo/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package br.com.eduardo.dscatalog.resources.exceptions;
 
-import br.com.eduardo.dscatalog.services.exceptions.EntityNotFoundException;
+import br.com.eduardo.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,8 +12,8 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
         StandardError error = new StandardError();
         error.setTimestamp(Instant.now());
         error.setStatus(HttpStatus.NOT_FOUND.value());
--- backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/services/CategoryService.java
@@ -3,11 +3,12 @@ package br.com.eduardo.dscatalog.services;
 import br.com.eduardo.dscatalog.dto.CategoryDTO;
 import br.com.eduardo.dscatalog.entities.Category;
 import br.com.eduardo.dscatalog.repositories.CategoryRepository;
-import br.com.eduardo.dscatalog.services.exceptions.EntityNotFoundException;
+import br.com.eduardo.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,6 +21,7 @@ public class CategoryService {
     public CategoryService(CategoryRepository repo){
         this.repo = repo;
     }
+
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list = repo.findAll();
@@ -29,7 +31,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findByid(Long id) {
         Optional<Category> obj = repo.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Id not found."));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found."));
         return new CategoryDTO(entity);
     }
 
@@ -40,5 +42,17 @@ public class CategoryService {
         entity = repo.save(entity);
         return new CategoryDTO(entity);
 
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repo.getOne(id);
+            entity.setName(dto.getName());
+            entity = repo.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found " + id );
+        }
     }
 }
