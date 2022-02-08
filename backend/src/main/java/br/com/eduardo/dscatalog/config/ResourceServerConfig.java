eb11da6d346377fcac08b1bc9be78911680d06da
Eduardo Alves dos Santos <santosedu@live.com>
2022-02-08 19:51:23 -0300

    H2 security configuration

--- backend/src/main/java/br/com/eduardo/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/br/com/eduardo/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,9 @@
 package br.com.eduardo.dscatalog.config;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,16 +11,21 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    private static final String[] PUBLIC = {"/ouath/token"};
+    private static final String[] PUBLIC = {"/ouath/token", "/h2-console/**"};
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
     private static final String[] ADMIN = {"/users/**"};
 
     @Autowired
     private JwtTokenStore tokenStore;
+
+    @Autowired
+    private Environment environment;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -27,6 +34,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        //H2 - liberacao dos frames do banco
+        if(Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET,OPERATOR_OR_ADMIN).permitAll()
